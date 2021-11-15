accb4cf4e8f10985b9a273e279eccd19ebc2eb5f
efe3535 <a.efe.akyazi@gmail.com>
2021-11-15 23:37:03 +0300

    First version!

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-import config
 import requests
+import json
+from random import choice
+from colorama import Fore
 
-x = requests.get("https://api.github.com") # to be continued :]
+uname = input("Author of the repo?\t")
+reponame = input("Name of the repo?\t")
+
+x = requests.get(f"https://api.github.com/repos/{uname}/{reponame}/stargazers") # to be continued :]
+stargazer_names = []
+
+for element in json.loads(x.content):
+    stargazer_names.append(element["login"])
+
+luckyone = choice(stargazer_names)
+
+print("Luckiest person ever is:\t", Fore.GREEN, luckyone, Fore.RESET)
+print("GitHub link of this lucky person:\t",Fore.BLUE, f"https://github.com/{luckyone}", Fore.RESET)
