08b4da3ad313cd1caeaa50ddf4497afd834b9586
efe3535 <a.efe.akyazi@gmail.com>
2021-11-17 11:38:36 +0300

    Check if repo has enough stars.

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ from sys import argv
 
 uname = ""
 reponame = ""
+stargazers_count = int()
 
 if len(argv) == 1:
     print("tip:\tyou can always use arguments like \"--uname --reponame --u --r -u -r\"")
     uname = input("Author of the repo?\t")
     reponame = input("Name of the repo?\t")
+
+if("--help" in argv):
+    print("usage:\n--help:\tshows help\n--uname:\tusername for repo\n--reponame:\trepo name for repo")
+    exit(0)
 
 else:
     c = 0
@@ -39,17 +44,27 @@ if(uname == "" and reponame == ""):
     exit(1)
 
 x = requests.get(f"https://api.github.com/repos/{uname}/{reponame}/stargazers") # to be continued :]
-if x.status_code != 404:
+y = requests.get(f"https://api.github.com/repos/{uname}/{reponame}"); 
+
+if(y.status_code != 404):
+    stargazers_count = json.loads(y.content)["stargazers_count"]
+
+if x.status_code != 404 and stargazers_count > 1: 
     stargazer_names = []
 
     for element in json.loads(x.content):
         stargazer_names.append(element["login"])
-
+     
     luckyone = choice(stargazer_names)
 
     print("Luckiest person ever is:\t", Fore.GREEN, luckyone, Fore.RESET)
     print("GitHub link of this lucky person:\t",Fore.BLUE, f"https://github.com/{luckyone}", Fore.RESET)
 
 else:
-    print("This user / repo doesn't exist.")
-    exit(1)
+    if(stargazers_count == 0 and x.status_code != 404):
+        print(f"This repo doesn't have enough stars for a giveaway... {Fore.BLUE}:({Fore.RESET}") 
+
+    elif(x.status_code == 404):
+        print(f"{Fore.RED}This user / repo doesn't exist.{Fore.RESET}")
+        exit(1)
+
